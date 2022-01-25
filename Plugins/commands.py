3f972378940cd6e2307c31835383f1a6169bf574
pulikesimeme <95121013+pulikesimeme@users.noreply.github.com>
2022-01-25 13:10:30 +0530

    Initial commit

---
+++ Plugins/commands.py
@@ -0,0 +1,230 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import pyrogram
+from pyrogram import filters
+from bot import autocaption
+from config import Config
+from database.database import *
+from translation import Translation
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+ 
+
+#all buttons 
+
+#start buttons 
+
+start_button=InlineKeyboardMarkup(
+        [
+              [
+                  InlineKeyboardButton("📄 BOT STATUS", callback_data = "status_data")
+              ], 
+              [
+                  InlineKeyboardButton("📫 UPDATES", url = "https://t.me/PM_Bots"), 
+                  InlineKeyboardButton("📕 ABOUT", callback_data = "about_data")
+              ], 
+              [
+                  InlineKeyboardButton("💡 HELP", callback_data = "help_data"), 
+                  InlineKeyboardButton("🔐 CLOSE", callback_data = "close_data")
+              ] 
+        ]
+)
+
+# help buttons
+
+help_button=InlineKeyboardMarkup(
+        [
+              [
+                InlineKeyboardButton("ABOUT MARKDOWN", callback_data = "markdown_data")
+              ], 
+              [
+                  InlineKeyboardButton("⬇️ BACK", callback_data = "back_data"), 
+                  InlineKeyboardButton("🔐 CLOSE", callback_data = "close_data")
+              ]
+        ]
+)
+ 
+# about button 
+
+about_button=InlineKeyboardMarkup(
+        [
+              [
+                  InlineKeyboardButton("⬇️ BACK", callback_data = "back_data"), 
+                  InlineKeyboardButton("🔐 CLOSE", callback_data = "close_data")
+              ]
+        ]
+) 
+
+
+@autocaption.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME), 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "markdown",
+          disable_web_page_preview = True, 
+          reply_markup = start_button
+      )
+
+
+@autocaption.on_message(filters.command("help") & filters.private)
+async def help(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.HELP_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "html",
+          disable_web_page_preview = True,
+          reply_markup = help_button           
+      )
+
+
+@autocaption.on_message(filters.command("about") & filters.private)
+async def about(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.ABOUT_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "markdown",
+          disable_web_page_preview = True, 
+          reply_markup = about_button
+      )   
+
+
+@autocaption.on_message(filters.command("set_caption") & filters.private)
+async def set_caption(bot, cmd):
+    if Config.ADMIN_ID != cmd.from_user.id:
+        return
+
+    if len(cmd.command) == 1:
+        await cmd.reply_text(
+            "🖊️ 𝐒𝐄𝐓 𝐂𝐀𝐏𝐓𝐈𝐎𝐍 \n\nUse this command to set your own caption text \n\n👉 `set_caption My Caption`", 
+            quote = True
+        )
+    else:
+        command, caption = cmd.text.split(' ', 1)
+        await update_caption(cmd.from_user.id, caption)
+        await cmd.reply_text(f"**--Your Caption--:**\n\n{caption}", quote=True)
+
+
+
+
+# call_backs 
+
+@autocaption.on_callback_query()
+async def button(bot, cmd: CallbackQuery):
+    cb_data = cmd.data
+    if "about_data" in cb_data:
+        await cmd.message.edit(
+             text = Translation.ABOUT_TEXT,
+             parse_mode="markdown", 
+             disable_web_page_preview=True, 
+             reply_markup=InlineKeyboardMarkup(
+                 [
+                     [
+                      InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                      InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                     ]
+ 
+                 ] 
+             ) 
+        )
+    elif "help_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.HELP_TEXT,
+               parse_mode="html", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [
+                        InlineKeyboardButton("ABOUT MARKDOWN", callback_data = "markdown_data")
+                       ],
+                       [
+                        InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                        InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                       ]
+ 
+                   ] 
+               ) 
+          )
+    elif "back_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME),
+               parse_mode="markdown", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                      
+                       [
+                        InlineKeyboardButton("📄 BOT STATUS", callback_data = "status_data")
+                       ], 
+                       [
+                        InlineKeyboardButton("📫 UPDATES", url="https://t.me/PM_Bots"),
+                        InlineKeyboardButton("📕 ABOUT ME", callback_data="about_data")
+                       ],
+                       [
+                        InlineKeyboardButton("💡 HELP", callback_data="help_data"),
+                        InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                       ]
+                   ]
+               )
+          )
+    elif "close_data" in cb_data:
+          await cmd.message.delete()
+          await cmd.message.reply_to_message.delete()
+
+    elif "markdown_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.MARKDOWN_TEXT,
+               parse_mode="html", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [
+                        InlineKeyboardButton("⬇️ BACK", callback_data="help_data"),
+                        InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                       ]
+ 
+                   ] 
+               ) 
+          )
+    elif "status_data" in cb_data:
+          if Config.ADMIN_ID == int(cmd.message.chat.id):
+             try:
+                caption = await get_caption(cmd.from_user.id)
+                caption_text = caption.caption
+             except:
+                caption_text = "Not Added" 
+             await cmd.message.edit(
+                  text=Translation.STATUS_DATA.format(caption_text, Config.CAPTION_POSITION),
+                  parse_mode="html", 
+                  disable_web_page_preview=True, 
+                  reply_markup=InlineKeyboardMarkup(
+                      [
+                          [
+                           InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                           InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                          ]
+ 
+                      ] 
+                  ) 
+             )
+          else:
+             await cmd.message.edit(
+                  text=Translation.NOT_ADMIN_TEXT,
+                  parse_mode="html", 
+                  disable_web_page_preview=True, 
+                  reply_markup=InlineKeyboardMarkup(
+                      [
+                          [
+                           InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                           InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                          ]
+ 
+                      ] 
+                  ) 
+             )
+ 
---
+++ README.md
@@ -0,0 +1,65 @@
+# AutoCaption-Ts_Bot
+
+
+An open source of telegram Auto Caption bot
+
+- For demo you can check the <b>[AutoCaption_Ts_Bot](https://telegram.me/AutoCaption_Ts_Bot)</b>
+
+# About Bot
+
+This is a light weight and fastest Auto caption bot which can add caption prescribed by the admin of bot
+
+<i>( Works only in private or public Channel )</i> 
+
+### Commands
+
+```
+ /start - A command to start the bot
+ /help  - A command to help you how to use the bot
+ /about - A command to know about the bot
+ /set_caption - A command to set your caption text
+```
+#### Buttons
+
+```
+Admin Only
+
+Bot Status - To know the current caption text and position of caption you provided.
+```
+# Deployment
+
+## About Heroku Config Vars
+
+```
+CAPTION_POSITION - Position of your caption
+
+(Top or bottom or nil) 
+
+(nil - will overwrite the caption)
+```
+You can deploy the bot using heroku
+
+- 🥰 Fork it and deploy
+ 
+- 👇 Press the deploy button.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/pulikesimeme/AutoCaption-Ts_Bot-Clone?organization=pulikesimeme&organization=pulikesimeme)
+
+## Please note
+
+I created this bot because it might be helpful for Channel Admins/Owners, so they have no need to edit all files
+
+I am not a progrommer though it is written in my own hands 
+If you have any suggestions to update the bot for better user friendly expirience you can contact me 
+Through [@Ts_Bots](https://telegram.me/Ts_bots) or you can contact me directly.
+
+## My Channel
+
+<b>[@Ts_Bots](https://telegram.me/ts_bots)</b>
+
+
+### Thanks to
+
+<i>❤️ Thanks to everyone in this journey</i> 
+
+<i>🥰 Thanks to [Anonymous](https://telegram.me/Ns-AnoNymouS) for helping me in all the ways</i> 
